b6a00ff18fb01a7882cb6d5fab3c578674f375c6
Toir Makhamadjonov <110101406+realmugen13@users.noreply.github.com>
2022-11-20 15:12:20 +0500

    Add files via upload

---
+++ portfolio/app.js
@@ -0,0 +1,8 @@
+<script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
+
+var typed = new Typed('.input', {
+    strings: [' Toir Makhamadjonov.', 'Web Developer.', 'UI / UX Designer.'],
+    typeSpeed: 70,
+    backSpeed: 60,
+    loop:true
+});
